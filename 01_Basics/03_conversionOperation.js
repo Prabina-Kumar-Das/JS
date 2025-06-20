40a67fca66a301c06eb94be1303cf765322cf512
Prabina Kumar Das <119826280+Prabina-Kumar-Das@users.noreply.github.com>
2025-06-20 06:23:31 +0000

    postfix & prefix

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -15,8 +15,36 @@
 // let booleanIsLoggedIn = Boolean(IsLoggedIn);
 // console.log(booleanIsLoggedIn)
 
-let someNumber=null
+// let someNumber=null
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
 
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+let value =3
+let negValue=-value
+// console.log(negValue);
+
+let str1 = "Prabina"
+let str2 = " Das"
+let str3 = str1-str2
+// console.log(str3)
+
+
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"1")
+
+// console.log(true)
+
+let x=1;
+const y=x++;
+console.log(`x:${x} , y:${y}`);
+
+let a=1;
+const b=++a;
+console.log(`a:${a} , b:${b}`);
+
+
+
+

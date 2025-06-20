f9ad94e1c4aedd29aa9fd29fd63792d5f9103314
Prabina Kumar Das <119826280+Prabina-Kumar-Das@users.noreply.github.com>
2025-06-20 05:11:10 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+// let score = true;
+
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+// // console.log(score); 
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let IsLoggedIn = 5;
+// let booleanIsLoggedIn = Boolean(IsLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+let someNumber=null
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)

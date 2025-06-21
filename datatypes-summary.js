f2b33ca660f41f24d2f1a16b2b71abcd9e3c9bbc
Prabina Kumar Das <119826280+Prabina-Kumar-Das@users.noreply.github.com>
2025-06-21 05:00:11 +0000

    Datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,25 @@
+// Primitive
+// 7 types: String, Number, Boolean, Null, Undefiend, Symbol, BigInt
+
+// const id = Symbol(123)
+// const anotherId = Symbol(123)
+
+// console.log(id === anotherId);
+
+// const bigNumber = 345346346346364364363634643636n
+// console.log(bigNumber);
+
+const heros = ["Shaktiman", "nagraj", "doga"];
+let myObj = {
+    name: "Prabina",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+console.log(myFunction);
+console.log(typeof myObj);
+
+

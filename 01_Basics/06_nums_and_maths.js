9bd6c5b4f8a0b91351e0848fd30382afc8e2361b
Prabina Kumar Das <119826280+Prabina-Kumar-Das@users.noreply.github.com>
2025-06-22 06:36:40 +0000

    Numbers of JS

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,24 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+
+// console.log(typeof(balance));
+
+console.log(balance.toFixed(2));
+
+
+const otherNumber = 23.4335
+console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+

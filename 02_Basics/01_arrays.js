356f2edd7c53b510b5e894e549ceef57d7414e14
Prabina Kumar Das <119826280+Prabina-Kumar-Das@users.noreply.github.com>
2025-06-25 05:50:53 +0000

    array01

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,11 @@
+const myArr = [0, 1, 2, 3, 4];
+
+myArr.push(6)
+myArr.pop()
+myArr.unshift(9)
+myArr.shift()
+console.log(myArr);
+
+const newArr = myArr.join()
+console.log(myArr)
+console.log(newArr);
